cdfd445a0f151a19203a7d3de8558546f747582b
Isaac Lyons <isaac@snowskeleton.net>
2023-05-10 16:22:52 -0400

    better examples

--- Snowclock/GetSounds.swift
+++ Snowclock/GetSounds.swift
@@ -10,22 +10,32 @@ func getSounds() -> [String]{
     ///File Manager alows us access to the device's files to which we are allowed.
     let fileManager: FileManager = FileManager()
     
-    let rootSoundDirectory = "Sounds"
-    let newDirectory: NSMutableDictionary = [
-        "path" : "\(rootSoundDirectory)",
-        "files" : [] as [String]
-    ]
-    /**
-     For each directory, it looks at each item (file or directory) and only appends the sound files to the soundfiles[i]files array.
-     
-     - URLs: All of the contents of the directory (files and sub-directories).
-     */
-    let directoryURL: URL = URL(
-        fileURLWithPath: newDirectory.value(forKey: "path") as! String,
-        isDirectory: true
-    )
+//    let rootSoundDirectory = "/Library/Application Support/Sounds"
+//    let newDirectory: NSMutableDictionary = [
+//        "path" : "\(rootSoundDirectory)",
+//        "files" : [] as [String]
+//    ]
+//    /**
+//     For each directory, it looks at each item (file or directory) and only appends the sound files to the soundfiles[i]files array.
+//
+//     - URLs: All of the contents of the directory (files and sub-directories).
+//     */
+//    let directoryURL: URL = URL(
+//        fileURLWithPath: newDirectory.value(forKey: "path") as! String,
+//        isDirectory: true
+//    )
     
-    var URLs: [URL]?
+    let url = Bundle.main.url(forResource: "Sounds", withExtension: "json")
+    let data = try Data(contentsOf: url)
+    let configuration = try jsonDecoder.decode(Configuration.self, from: data)
+    let directoryURL = try FileManager.default.url(
+        for: .applicationSupportDirectory,
+        in: .userDomainMask,
+        appropriateFor: nil,
+        create: false)
+    
+    
+    var URLs: [URL] = []
     do {
         URLs = try fileManager.contentsOfDirectory(
             at: directoryURL,
@@ -37,7 +47,7 @@ func getSounds() -> [String]{
     }
     var urlIsaDirectory: ObjCBool = ObjCBool(false)
     var soundPaths: [String] = []
-    for url in URLs! {
+    for url in URLs {
         fileManager.fileExists(
             atPath: url.path,
             isDirectory: &urlIsaDirectory
